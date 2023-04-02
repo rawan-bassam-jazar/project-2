c92ecb1f492706b0c2d77e89bc6affc669f38882
rawan-bassam-jazar <rawan.jazar@yahoo.com>
2023-04-02 15:13:46 +0300

    test

---
+++ js files/homepage.js
@@ -0,0 +1,11 @@
+ 
+// home page file
+var names=["روان بسام عبدالرؤوف جزر","سمية رائد احمد عوده" ,"دانا عصام ابراهيم الدعجه"];
+var ids=["0198032","0183452","0178034"];
+
+var nameuser=document.getElementById("name-active");
+var userpageName=document.getElementById("userpage-name");
+
+var user=`${ids[0]} ${names[0]}`
+ nameuser.innerHTML=user ;
+ userpageName.innerHTML=user;
